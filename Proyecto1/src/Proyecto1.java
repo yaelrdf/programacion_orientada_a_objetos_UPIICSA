9393177a92d0058a32f512c69a38188d9ea924c6
yaelrdf <mc.minion2005@hotmail.com>
2025-02-25 10:18:00 -0600

    it works

--- Proyecto1/src/Proyecto1.java
+++ Proyecto1/src/Proyecto1.java
@@ -1,7 +1,5 @@
 public class Proyecto1 {
-    public static void main(String[] args){
-        
+    public static void main(String[] args) {
+        Persona objPersona1 = new Persona("Yael", "Saldana", "Flores");
     }
 }
-
-
