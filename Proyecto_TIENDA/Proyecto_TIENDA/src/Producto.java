851689f6347675ab11fb657d464bb77581ad2a00
yaelrdf <mc.minion2005@hotmail.com>
2025-03-11 10:39:41 -0600

    hotfix

--- Proyecto_TIENDA/Proyecto_TIENDA/src/Producto.java
+++ Proyecto_TIENDA/Proyecto_TIENDA/src/Producto.java
@@ -7,11 +7,23 @@ class Producto {
     private double precioVenta;
     private double contenidoNeto;
     private String proveedor;
-    private int cantidadAlmacen;
-    private tipoEmpaque Empaque;
+    private String cantidadAlmacen;
+    private tipoEmpaque empaque;
     private int stockMinimo;
     private int codigoBarras;
-}
 
- //Constructor
- public Producto(double precioCompra, String marca, String nombre, String caducidad, double precioVenta, double contenidoNeto, String proveedor, String cantidadAlmacen, tipoEmpaque empaque, int stockMinimo, String codigoBarras){}
+     //Constructor
+    public Producto(double precioCompra, String marca, String nombre, String caducidad, double precioVenta, double contenidoNeto, String proveedor, String cantidadAlmacen, tipoEmpaque empaque, int stockMinimo, int codigoBarras){
+        this.precioCompra = precioCompra;
+        this.marca = marca;
+        this.nombre = nombre;
+        this.caducidad = caducidad;
+        this.precioVenta = precioVenta;
+        this.contenidoNeto = contenidoNeto;
+        this.proveedor = proveedor;
+        this.cantidadAlmacen = cantidadAlmacen;
+        this.empaque = empaque;
+        this.stockMinimo = stockMinimo;
+        this.codigoBarras = codigoBarras;
+    }
+}
