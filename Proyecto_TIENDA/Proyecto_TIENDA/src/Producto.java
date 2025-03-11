ef5f996a03128c66c26d8856364d56fc6af78bcb
yaelrdf <mc.minion2005@hotmail.com>
2025-03-11 10:36:09 -0600

    fix

---
+++ Proyecto_TIENDA/Proyecto_TIENDA/src/Producto.java
@@ -0,0 +1,17 @@
+class Producto {
+    //Exposicion de atributos
+    private double precioCompra;
+    private String marca;
+    private String nombre;
+    private String caducidad;
+    private double precioVenta;
+    private double contenidoNeto;
+    private String proveedor;
+    private int cantidadAlmacen;
+    private tipoEmpaque Empaque;
+    private int stockMinimo;
+    private int codigoBarras;
+}
+
+ //Constructor
+ public Producto(double precioCompra, String marca, String nombre, String caducidad, double precioVenta, double contenidoNeto, String proveedor, String cantidadAlmacen, tipoEmpaque empaque, int stockMinimo, String codigoBarras){}
---
+++ Proyecto_TIENDA/Proyecto_TIENDA/src/tipoEmpaque.java
@@ -0,0 +1,3 @@
+public enum tipoEmpaque {
+    bolsa,lata,frasco
+}
